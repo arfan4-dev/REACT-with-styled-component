d3a8cc36037e807589e6548f3c507758fa251588
MUHAMMAD ARFAN <106101890+fanijan786@users.noreply.github.com>
2022-11-08 11:03:01 -0800

    'commit'

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
 import logo from './logo.svg';
-import './App.css';
+// import './App.css';
+import styled, {css} from 'styled-components'
 
+const Button=styled.button`
+background-color:"red";
+color:"white";
+margin:10px;
+  font-size:${({priamry})=>(priamry?"50px":"20px")};
+  padding:5px;
+  display:inline-block;
+  border:5px solid green;
+  ${({primary})=>(primary && css `
+  
+  background-color:green;
+  color:#fff
+  `)};
+${({secondary})=>(secondary && css `
+  
+  background-color:pink;
+  color:white
+  `)};
+  `;
+const bigButton=styled(Button)`
+font-size:3rem;
+`;
+ 
+
+
+
+const Link=styled.a.attrs({
+  target:"_blank"
+})`
+`
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Link target="" href="https://www.google.com">This is Link</Link>
+    {/* <Button primary>primary</Button>
+     <Button secondary>Secondary</Button>
+     <bigButton secondary>BigButton</bigButton>
+     <bigButton primary>BigButton</bigButton> */}
+    {/* <DIV>a</DIV> */}
+
     </div>
   );
 }
